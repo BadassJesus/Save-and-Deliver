8cc118409f9f235afe0f97c7e5522f3addb2a450
githubjeka <et.coder@gmail.com>
2016-04-22 16:45:19 +0300

    :pray:

---
+++ Lord.php
@@ -0,0 +1,60 @@
+<?php
+namespace BadassJesus\SaveDeliver;
+
+final class Lord
+{
+    /**
+     * I am the
+     * @var Lord;
+     * and there is none else.
+     */
+    private static $triunity;
+
+    /**
+     * @var string
+     */
+    private $something;
+
+    /**
+     * The Power of the Prayer
+     * @return Lord will always hear us and do whatever is necessary for our real need.
+     */
+    public static function answerMe()
+    {
+        if (is_null(self::$triunity)) {
+            self::$triunity = new self();
+        }
+
+        return self::$triunity;
+    }
+
+    public function healThe(string $something) : Lord
+    {
+        $this->something = htmlspecialchars($something, ENT_COMPAT | ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5);
+
+        return $this;
+    }
+
+    /**
+     *  Now and ever, and to the ages of ages.
+     */
+    public function amen()
+    {
+        return $this->something;
+    }
+
+    public function __construct()
+    {
+        throw new \LogicException('Ask, and it will be given to you;');
+    }
+
+    public function __clone()
+    {
+        throw new \LogicException('Seek, and you will find;');
+    }
+
+    public function __wakeup()
+    {
+        throw new \LogicException('Knock, and it will be opened to you.');
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# salva-et-serva
+```php
+<?= 
+Lord::answerMe()
+    ->healThe($myHtml)
+    ->amen(); 
+?>
+```
